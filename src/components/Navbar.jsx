cdf723f77e2ee1c1b402b05ce7214445e9bb9552
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-09-13 01:19:26 +0600

    react routing pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,27 @@
 import { Link } from "react-router-dom";
+import logoIMG from"../assets/logo.png";
 
 function Navbar() {
   return (
-    <nav className="flex gap-5">
-      <Link to="/">Home</Link>
+    < >
+    <nav id="Navbar ">
+        <div className="container">
+        <div className="Nav_Menu pt-[43px]  pb-[38px] ">
+<Link className="logo w-[127px]">
+    <img src={logoIMG} alt="Brand logo"  />
+</Link>
 
-      <Link to="/about">About</Link>
 
-      <Link to="/contact">Contact</Link>
-  
+
+
+        </div>
+
+
+        </div>
     </nav>
+    
+  
+    </>
   );
 }
 
